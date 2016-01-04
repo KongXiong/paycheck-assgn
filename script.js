4b6c2bc0d37646e4cddfbd247dad5e6b6dcc0aed
kongxiong <kxrules@gmail.com>
2016-01-04 11:42:50 -0600

    things

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+var hourlyRate;
+var hoursWorked;
+var pay;
+hourlyRate = 18;
+hoursWorked = 40;
+pay = hourlyRate * hoursWorked;
+
+console.log("This week your pay is $" + pay);
+
+
+var rate;
+var hours;
+var check;
+rate = prompt("what's pay?");
+hours = prompt("what's hours?");
+
+check = rate * hours;
+
+console.log(check);
+
+
+
+// var rate;
+// var hours;
+// var check;
+// rate = function(yourPay){
+// 	prompt("your rate?");
+// };
+// hours = function(yourHours){
+// 	prompt("your hours");
+// };
+// check = rate * hours;
+
+// console.log(check);
+
+// rate(18);
+// hours(40);
